2362a80e6ba3323804c6a1e79843701880dc3ec8
Esteban <esteban.morauc@gmail.com>
2025-05-30 18:08:49 -0400

    creacion del microservicio

---
+++ src/main/java/com/grupo6/servicioReputacion/ServicioReputacionApplication.java
@@ -0,0 +1,13 @@
+package com.grupo6.servicioReputacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioReputacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioReputacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo6/servicioReputacion/ServicioReputacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo6.servicioReputacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioReputacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
